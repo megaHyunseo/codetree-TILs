a7a77f762ddfbe0a0034b7ef0466b04ffd62caf7
megaHyunseo <97423664+megaHyunseo@users.noreply.github.com>
2023-12-18 09:39:35 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231218/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    double n;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cin >> n;
+
+    cout << n * 30.48;
+    
+    return 0;
+}
