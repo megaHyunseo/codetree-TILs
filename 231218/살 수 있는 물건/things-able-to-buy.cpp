a8e4a5c1697d17745d0bbb20f85ba3791493d593
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2023-12-18 22:23:44 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231218/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+
+    cin >> n;
+
+    if(n >= 3000) {
+        cout << "book";
+    }
+    else if(n >= 1000) {
+        cout << "mask";
+    }
+    else {
+        cout << 0;
+    }
+    return 0;
+}
