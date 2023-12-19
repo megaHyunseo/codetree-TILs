6db3a4fbb26d80d5e7a18d5c94b88d4378cf0a51
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2023-12-19 11:01:46 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231219/비교 연산/comparison-operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << (a >= b) << "\n";
+    cout << (a > b) << "\n";
+    cout << (b >= a) << "\n";
+    cout << (b > a) << "\n";
+    cout << (a == b) << "\n";
+    cout << (a != b);
+    return 0;
+}
