7d07c61ae135f6333ba65a76ee873c74769c6159
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2023-12-19 11:05:56 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231219/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a;
+    int b, c, d, e;
+
+    cin >> a;
+    cin >> b >> c >> d >> e;
+
+    cout << (a > b) << "\n";
+    cout << (a > c) << "\n";
+    cout << (a > d) << "\n";
+    cout << (a > e);
+    return 0;
+}
