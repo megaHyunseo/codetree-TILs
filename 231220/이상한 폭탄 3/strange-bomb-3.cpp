253573a8965843b167f01022d654059f6fd9b536
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2023-12-20 20:16:03 +0900

    Title: 이상한 폭탄 3, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 231220/이상한 폭탄 3/strange-bomb-3.cpp
+++ 231220/이상한 폭탄 3/strange-bomb-3.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #define MAX_N 100
+#define MAX_BOMB 1000000
 using namespace std;
 
 int n, k;
 int arr[MAX_N];
+bool explode[MAX_N];
+int bomb[MAX_BOMB];
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -12,19 +15,33 @@ int main() {
         cin >> arr[i];
     }
 
-    int ans = 0;
     for(int i=0; i<n; i++){
-        int cnt = 0;
-        for(int j=0; j<n; j++){
-            if(i == j) continue;
-
-            if(arr[i] == arr[j] && i-j <= k){
-                cnt++;
+        for(int j=i+1; j<n; j++){
+            if(j-i > k){
+                break;
+            }
+            if(arr[i] != arr[j]){
+                continue;
+            }
+            if(explode[i] == false){
+                bomb[arr[i]]++;
+                explode[i] = true;
+            }
+            if(explode[j] == false){
+                bomb[arr[j]]++;
+                explode[j] = true;
             }
         }
-        ans = max(ans, cnt);
     }
 
-    cout << ans;
+    int max_val = 1;
+    int max_idx = 0;
+    for(int i=0; i<MAX_BOMB; i++){
+        if(max_val <= bomb[i]){
+            max_val = bomb[i];
+            max_idx = i;
+        }
+    }
+    cout << max_idx;
     return 0;
 }
