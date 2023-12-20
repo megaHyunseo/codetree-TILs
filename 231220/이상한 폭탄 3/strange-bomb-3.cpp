295a5ee4ae896482f57c068efc76d401570131ea
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2023-12-20 18:08:35 +0900

    Title: 이상한 폭탄 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231220/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#define MAX_N 100
+using namespace std;
+
+int n, k;
+int arr[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> k;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        int cnt = 0;
+        for(int j=i; j<i+k; j++){
+            if(arr[i] == arr[j]){
+                cnt++;
+            }
+        }
+        ans = max(ans, cnt);
+    }
+
+    cout << ans;
+    return 0;
+}
