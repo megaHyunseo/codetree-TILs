0587af44ba1b628dd233729cb65163c54b1556ac
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2023-12-24 16:22:37 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231224/n부터 1까지 숫자 출력/print-n-to-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    int i;
+
+    cin >> n;
+    
+    i = n;
+
+    while(i >= 1) {
+        cout << i << " ";
+        i--; 
+    }
+    return 0;
+}
