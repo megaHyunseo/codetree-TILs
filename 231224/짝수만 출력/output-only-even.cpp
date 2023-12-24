170fd2634fefd450ed848c8fadaa2723590e311f
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2023-12-24 16:19:48 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231224/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+    int i;
+
+    cin >> a >> b;
+    i = a;
+
+    while(i <= b) {
+        cout << i << " ";
+        i += 2;
+    }
+    return 0;
+}
