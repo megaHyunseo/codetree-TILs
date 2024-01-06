a8d8a650a0e6c21adcacb2e153828ac551914cfb
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-01-06 21:13:12 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240106/두 선분/two-lines.cpp
+++ 240106/두 선분/two-lines.cpp
@@ -14,11 +14,15 @@ int main() {
         cout << "intersecting";
     }
     */
-    10 13 15 17
+
     //겹치는 경우로 생각하기
-    if(x2 > x3){
+    if(x2 > x3 && x4 > x2){
         cout << "intersecting";
-    }else if(x2 > x4){
+    }else if(x2 > x4 && x1 > x3){
+        cout << "intersecting";
+    }else if(x4 < x1 && x2 > x4){
+        cout << "intersecting";
+    }else if(x2 > x3 && x3 > x1){
         cout << "intersecting";
     }else{
         cout << "nonintersecting";
