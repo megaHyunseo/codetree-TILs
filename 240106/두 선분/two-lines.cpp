2ad4d4970a995f53b16203c86738af69238ca18c
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-01-06 20:57:16 +0900

    Title: 두 선분, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240106/두 선분/two-lines.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int x1, x2, x3, x4;
+
+int main() {
+    cin >> x1 >> x2 >> x3 >> x4;
+    //안 겹치는 경우로 생각하기
+    
+    /*
+    if(x2 < x3 || x4 < x1){
+        cout << "nonintersecting";
+    }else{
+        cout << "intersecting";
+    }
+    */
+
+    //겹치는 경우로 생각하기
+    if(x2 > x3){
+        cout << "intersecting";
+    }else if(x2 > x4){
+        cout << "intersecting";
+    }else if(x3 > x2){
+        cout << "intersecting";
+    }else if(x4 > x2){
+        cout << "intersecting";
+    }else{
+        cout << "nonintersecting";
+    }
+    return 0;
+}
