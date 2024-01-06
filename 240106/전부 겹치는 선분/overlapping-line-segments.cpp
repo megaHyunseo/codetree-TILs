863c72c5a58997594cfa98557d40a6e27b98ddf4
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-01-06 21:51:42 +0900

    Title: 전부 겹치는 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240106/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int x1, x2;
+int max_x1, min_x2;
+
+int main() {
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> x1 >> x2;
+        max_x1 = max(max_x1, x1);
+        min_x2 = min(min_x2, x2);
+    }
+
+    if(max_x1 <= min_x2){
+        cout << "Yes";
+    }else{
+        cout << "No";
+    }
+    
+    return 0;
+}
