28022177b46cf5ffd7de17bf04f2a7f5067cf5d3
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-01-07 14:44:27 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240107/코딩톡/coding-talk.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define MAX_M 100
+#define MAX_N 26
+
+using namespace std;
+
+int n, m, p;
+char c[MAX_M+1];
+int u[MAX_M+1];
+int check[MAX_N+1];
+
+int main() {
+    cin >> n >> m >> p;
+    for(int i = 0; i < m; i++){
+        cin >> c[i] >> u[i];
+    }
+
+    for(int i = p - 1; i < m; i++){
+        char x = c[i];
+        check[(int)(x - 65)] = 1;
+    }
+    
+    for(int i = 0; i < n; i++){
+        if(check[i] == 0){
+            cout << (char)(i + 65) << " ";
+        }
+    }
+    return 0;
+}
