9f29ad62700b502335e297c55476a60878d37800
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-01-07 14:01:17 +0900

    Title: 비둘기와 전기줄, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240107/비둘기와 전기줄/pigeons-and-electric-cords.cpp
+++ 240107/비둘기와 전기줄/pigeons-and-electric-cords.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <algorithm>
 
+#define MAX_N 100
+#define MAX_NUM 10
+#define UNDEFINED -1
+
 using namespace std;
 
 int n;
-bool arr[11] = {false, };
+int pigeon[MAX_N+1], move_dir[MAX_N+1], pos[MAX_NUM+1];
 
 int main() {
     cin >> n;
-    int x, y;
+    for(int i = 1; i <= n; i++){
+        cin >> pigeon[i] >> move_dir[i];
+    }
+    for(int i = 1; i <= MAX_NUM; i++){
+        pos[i] = UNDEFINED;
+    }
+
     int cnt = 0;
-    for(int i = 0; i < n; i++){
-        cin >> x >> y;
-        if(arr[x] == false){
-            arr[x] = true;
+    for(int i = 1; i <= n; i++){
+        if(pos[pigeon[i]] == UNDEFINED){
+            pos[pigeon[i]] = move_dir[i];   
         }else{
-            if(arr[x] != y){
+            if(pos[pigeon[i]] != move_dir[i]){
+                pos[pigeon[i]] = move_dir[i];
                 cnt++;
             }
         }
