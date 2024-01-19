68c2e689666ebbc58cd4675c786fbf0d309aa2cf
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-01-19 17:46:47 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+
+    cin >> a >> b;
+
+    for(int i = a ; i <= b ; ) {
+
+        cout << i << " ";
+        
+        if(i % 2 == 0) {
+            i += 3;
+        }
+        else {
+            i *= 2;
+        }
+    }
+    return 0;
+}
