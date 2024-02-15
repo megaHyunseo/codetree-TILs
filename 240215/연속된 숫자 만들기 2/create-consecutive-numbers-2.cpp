eeae04e062dce71a115263724d2cc88127dbaf0b
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-02-15 18:37:54 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240215/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240215/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -3,12 +3,14 @@
 
 using namespace std;
 
+int arr[3];
+
 int main() {
     for(int i = 0; i < 3; i++){
         cin >> arr[i];
     }
 
-    sort(arr, arr+3);
+    sort(arr, arr + 3);
     
     if(arr[0] + 1 == arr[1] && arr[1] + 1 == arr[2]){
         cout << 0;
@@ -17,6 +19,6 @@ int main() {
     }else{
         cout << 2;
     }
-    
+
     return 0;
 }
