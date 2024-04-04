f16148d126624221a66b01fd3eb0bf2cdd12745a
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-04-04 19:49:37 +0900

    Title: 홀수만 더하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240404/홀수만 더하기/add-only-odd-numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int sum = 0;
+    int n;
+    int a;
+
+    cin >> n;
+
+    for(int i = 0 ; i < n ; i++) { 
+
+        cin >> a;
+
+        if a%2==1 && a%3==0:
+            sum += a
+    }
+
+    cout << sum;
+
+    return 0;
+}
