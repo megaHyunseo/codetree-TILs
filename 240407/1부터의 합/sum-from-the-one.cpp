1cbf6a929bd9f4cd43cbabc18bb34ec02a104d85
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-04-07 13:51:44 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240407/1부터의 합/sum-from-the-one.cpp
+++ 240407/1부터의 합/sum-from-the-one.cpp
@@ -8,14 +8,14 @@ int main() {
     cin >> n;
 
     int sum = 0;
-    for(int i = 1 ; i <= n ; i++) {
-        if (sum > n) {
-            cout << i-1;
-            break;
-        }
+    for(int i = 1 ; i <= 100 ; i++) {
 
         sum += i;
-    }
 
+        if (sum >= n) {
+            cout << i;
+            break;
+        }
+    }
     return 0;
 }
