249e6d5ad0a44de2be925b04b5a0c58efc172898
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-08-28 11:00:57 +0900

    Title: 완전수, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240828/완전수/perfect-number.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+
+cnt = 0
+
+for i in range(a, b + 1):
+    
+    sum = 0
+    for j in range(1, i):
+        if i % j == 0:
+            sum += j
+    if i == sum:
+        cnt += 1
+
+print(cnt)
