30b92e31bec3704fec5e8d3a58348e99176ffb90
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-08-30 12:00:37 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240830/시험 통과 여부 확인하기 2/verify-test-passed-2.py
+++ 240830/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -1,26 +1,12 @@
 n = int(input())
 
-a = sum(map(int, input().split())) / 4
-b = sum(map(int, input().split())) / 4
-c = sum(map(int, input().split())) / 4
-
 cnt = 0
-if a >= 60:
-    cnt += 1
-    print('pass')
-else:
-    print('fail')
-    
-if b >= 60:
-    cnt += 1
-    print('pass')
-else:
-    print('fail')
-    
-if c >= 60:
-    cnt += 1
-    print('pass')
-else:
-    print('fail')
+for i in range(n):
+    a = sum(map(int, input().split())) / 4
+    if a >= 60:
+        print('pass')
+        cnt += 1
+    else:
+        print('fail')
 
 print(cnt)
