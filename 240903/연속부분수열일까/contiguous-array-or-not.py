dd673fbc1504a40f332053b353d5019ddc9fc4b9
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-09-03 00:13:45 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240903/연속부분수열일까/contiguous-array-or-not.py
+++ 240903/연속부분수열일까/contiguous-array-or-not.py
@@ -3,7 +3,11 @@ n1, n2 = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-k = a.index(b[0])
+try:
+    k = a.index(b[0])
+except:
+    pass
+    
 cnt = 0
 for i, n in enumerate(b):
     if a[k + i] == n:
