afb6005374a977ab16624e2f6049c6b617f20066
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-09-03 14:16:59 +0900

    Title: n개의 숫자 중 최소, Time: 105ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/n개의 숫자 중 최소/min-of-n-num.py
+++ 240903/n개의 숫자 중 최소/min-of-n-num.py
@@ -1,5 +1,10 @@
 n = int(input())
 
-a = min(list(map(int, input().split())))
+a = list(map(int, input().split()))
 
-print(a)
+cnt = 0
+for i in range(n):
+    if a[i] == min(a):
+        cnt += 1
+
+print(min(a), cnt)
