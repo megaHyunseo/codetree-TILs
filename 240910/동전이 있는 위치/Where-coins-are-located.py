aba6b0da5cddc2e503e8ae2b14f47dbc15d8f83f
HyunSeo <97423664+megaHyunseo@users.noreply.github.com>
2024-09-10 20:41:48 +0900

    Title: 동전이 있는 위치, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240910/동전이 있는 위치/Where-coins-are-located.py
+++ 240910/동전이 있는 위치/Where-coins-are-located.py
@@ -1,13 +1,13 @@
 n, m = map(int, input().split())
 
 k = [
-    [0 for i in range(n)]
-    for i in range(n)
+    [0 for i in range(n + 1)]
+    for i in range(n + 1)
 ]
 
 for i in range(m):
     a, b = map(int, input().split())
-    k[a - 1][b - 1] = 1
+    k[a][b] = 1
 
 for i in range(n):
     for j in range(n):
